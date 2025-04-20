724cef484393e15d938b93a1568523b2d70f0c82
Medhya Goel <medhya@stanford.edu>
2025-04-20 06:32:56 +0000

    keep the line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,6 @@ export default function HomePage() {
     return yearB - yearA; // Sort in descending order
   });
 
-  const yearSpacing = 80;
-
   return (
     <main className="min-h-screen bg-[#fdfcf7] text-[#3a3a3a] font-serif px-6 py-12 md:px-20 max-w-6xl mx-auto space-y-16">
       <section className="space-y-4">
@@ -72,11 +70,11 @@ export default function HomePage() {
 
       <section className="flex space-x-8">
         {/* Timeline Section */}
-        <div class="-my-6 w-2/3">
+        <div className="-my-6 w-2/3">
           {sortedEvents.map((event, index) => (
             <div key={index} className="relative pl-8 sm:pl-32 py-6 group">
-              <div className="font-caveat font-medium text-2xl text-teal-500 mb-1 sm:mb-0">{event.category}</div>
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-border sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-teal-600 after:border-4 after:box-content after:border-[#fdfcf7] after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+              <div className="font-caveat font-medium text-2xl" style={{color: event.color}}>{event.category}</div>
+              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-border sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:border-4 after:box-content after:border-[#fdfcf7] after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5" style={{backgroundColor: event.color}}>
                 <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">{event.year || `${event.start} - ${event.end}`}</time>
                 <div className="text-xl font-bold text-slate-900">{event.title}</div>
               </div>
@@ -128,3 +126,4 @@ export default function HomePage() {
     </main>
   );
 }
+
